bbd3417f190c3c7b6e8993c84d4fcb133bff3590
MarcelinaBadBoy <64033143+MarcelinaBadBoy@users.noreply.github.com>
2022-12-30 15:14:17 +0100

    Added Foundry Smelter gui animations in JEI

--- src/main/java/io/github/marcelinebadboy/expansioncraft/integration/FoundrySmelterRecipeCategory.java
+++ src/main/java/io/github/marcelinebadboy/expansioncraft/integration/FoundrySmelterRecipeCategory.java
@@ -1,11 +1,16 @@
 package io.github.marcelinebadboy.expansioncraft.integration;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import io.github.marcelinebadboy.expansioncraft.ExpansionCraft;
 import io.github.marcelinebadboy.expansioncraft.init.BlockInit;
 import io.github.marcelinebadboy.expansioncraft.init.recipes.FoundrySmelterRecipe;
 import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.ITickTimer;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
+import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
@@ -22,10 +27,14 @@ public class FoundrySmelterRecipeCategory implements IRecipeCategory<FoundrySmel
 
     private final IDrawable background;
     private final IDrawable icon;
+    private final IDrawableAnimated fire;
+    private final IDrawableAnimated arrow;
 
     public FoundrySmelterRecipeCategory(IGuiHelper helper) {
         this.background = helper.createDrawable(TEXTURE, 4, 4, 168, 97);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(BlockInit.FOUNDRY_SMELTER.get()));
+        this.arrow = helper.drawableBuilder(TEXTURE, 177, 14, 23, 17).buildAnimated( 350, IDrawableAnimated.StartDirection.LEFT, false);
+        this.fire = helper.drawableBuilder(TEXTURE, 176, 0, 14, 14).buildAnimated( 700, IDrawableAnimated.StartDirection.TOP, true);
     }
 
     @Override
@@ -53,6 +62,9 @@ public class FoundrySmelterRecipeCategory implements IRecipeCategory<FoundrySmel
     public void setRecipe(IRecipeLayoutBuilder builder, FoundrySmelterRecipe recipe, IFocusGroup focuses) {
         if(recipe.getIngredients().size() == 1) {
             builder.addSlot(RecipeIngredientRole.INPUT, 35, 14).addIngredients(recipe.getIngredients().get(0));
+            builder.addSlot(RecipeIngredientRole.RENDER_ONLY, 53, 14).addItemStack(new ItemStack(Items.AIR));
+            builder.addSlot(RecipeIngredientRole.RENDER_ONLY, 35, 32).addItemStack(new ItemStack(Items.AIR));
+            builder.addSlot(RecipeIngredientRole.RENDER_ONLY, 53, 32).addItemStack(new ItemStack(Items.AIR));
 
             builder.addSlot(RecipeIngredientRole.OUTPUT, 113, 51).addItemStack(recipe.getResultItem());
         }
@@ -60,6 +72,8 @@ public class FoundrySmelterRecipeCategory implements IRecipeCategory<FoundrySmel
         if(recipe.getIngredients().size() == 2) {
             builder.addSlot(RecipeIngredientRole.INPUT, 35, 14).addIngredients(recipe.getIngredients().get(0));
             builder.addSlot(RecipeIngredientRole.INPUT, 53, 14).addIngredients(recipe.getIngredients().get(1));
+            builder.addSlot(RecipeIngredientRole.RENDER_ONLY, 35, 32).addItemStack(new ItemStack(Items.AIR));
+            builder.addSlot(RecipeIngredientRole.RENDER_ONLY, 53, 32).addItemStack(new ItemStack(Items.AIR));
 
             builder.addSlot(RecipeIngredientRole.OUTPUT, 113, 51).addItemStack(recipe.getResultItem());
         }
@@ -68,6 +82,7 @@ public class FoundrySmelterRecipeCategory implements IRecipeCategory<FoundrySmel
             builder.addSlot(RecipeIngredientRole.INPUT, 35, 14).addIngredients(recipe.getIngredients().get(0));
             builder.addSlot(RecipeIngredientRole.INPUT, 53, 14).addIngredients(recipe.getIngredients().get(1));
             builder.addSlot(RecipeIngredientRole.INPUT, 35, 32).addIngredients(recipe.getIngredients().get(2));
+            builder.addSlot(RecipeIngredientRole.RENDER_ONLY, 53, 32).addItemStack(new ItemStack(Items.AIR));
 
             builder.addSlot(RecipeIngredientRole.OUTPUT, 113, 51).addItemStack(recipe.getResultItem());
         }
@@ -82,5 +97,11 @@ public class FoundrySmelterRecipeCategory implements IRecipeCategory<FoundrySmel
         }
 
         builder.addSlot(RecipeIngredientRole.RENDER_ONLY, 44, 68).addItemStack(new ItemStack(Items.LAVA_BUCKET));
+    }
+
+    @Override
+    public void draw(FoundrySmelterRecipe recipe, IRecipeSlotsView recipeSlotsView, PoseStack matrixStack, double mouseX, double mouseY) {
+        this.arrow.draw(matrixStack, 77, 50);
+        fire.draw(matrixStack, 44, 51);
     }
 }
